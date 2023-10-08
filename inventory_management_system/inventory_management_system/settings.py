76aa3c969cd63275bcd1d187fac4c717a589875e
bamoya <43202585+bamoya@users.noreply.github.com>
2023-10-08 13:04:49 +0100

    Update settings.py

--- inventory_management_system/inventory_management_system/settings.py
+++ inventory_management_system/inventory_management_system/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'inventory_management_system.wsgi.application'
 DATABASES = {
     'default': {'ENGINE': 'django.db.backends.mysql',   
         'NAME': 'inventory_db',   
-        'USER': 'root',
-        'PASSWORD': 'BaMoYa2002',  
+        'USER': 'username',
+        'PASSWORD': 'password',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',   
         'OPTIONS': {   
